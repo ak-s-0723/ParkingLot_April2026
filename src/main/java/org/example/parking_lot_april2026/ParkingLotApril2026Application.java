10723c040d7b4fae4048e298bdf7c055e6e71593
khannaanurag1 <khannaanurag1@gmail.com>
2026-04-16 23:44:45 +0530

    initial commit

---
+++ src/main/java/org/example/parking_lot_april2026/ParkingLotApril2026Application.java
@@ -0,0 +1,13 @@
+package org.example.parking_lot_april2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApril2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotApril2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parking_lot_april2026/ParkingLotApril2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parking_lot_april2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApril2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
